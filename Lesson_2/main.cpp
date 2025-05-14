be06e26edd2b58cd818509cd6183fc7c9fa2511b
Mafin <116375324+Mafinesko@users.noreply.github.com>
2025-05-14 20:25:06 +0300

    Update main.cpp

--- Lesson_2/main.cpp
+++ Lesson_2/main.cpp
@@ -5,12 +5,12 @@ int main() {
     const int NUM_INPUTS = 5;
     std::vector<int> numbers(NUM_INPUTS);
 
-    std::cout << "Пожалуйста, введите " << NUM_INPUTS << " чисел, каждое на новой строке:" << std::endl;
+    std::cout << "Enter ammount " << NUM_INPUTS << " numbers, each on a new line:" << std::endl;
     for (int i = 0; i < NUM_INPUTS; ++i) {
         std::cin >> numbers[i];
     }
 
-    std::cout << "\nВот соответствующая фигура:" << std::endl;
+    std::cout << "Result:" << std::endl;
     for (int number : numbers) {
         for (int i = 0; i < number; ++i) {
             std::cout << "*";
